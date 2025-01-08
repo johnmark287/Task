be6c4848aa456cd3c44815d0e4250578ca7f3e29
Johnmark <117285113+johnmark287@users.noreply.github.com>
2025-01-08 14:45:47 +0000

    login and token functionalities

--- task_manager/taks/urls.py
+++ task_manager/taks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, RegisterUsetView
+from .views import TaskViewSet, RegisterUsetView, LoginView
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('register/', RegisterUsetView.as_view(), name='regiser')
+    path('register/', RegisterUsetView.as_view(), name='regiser'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- task_manager/taks/views.py
+++ task_manager/taks/views.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Task
 from .serializers import TaskSerializer, UserSerializer
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
 class TaskViewSet(viewsets.ModelViewSet):
@@ -20,6 +22,7 @@ class TaskViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
         
 class RegisterUsetView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = UserSerializer(data=request.data)
 
@@ -28,5 +31,16 @@ class RegisterUsetView(APIView):
             return Response({"message": "User created successfully."}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
 
+        return Response({
+            'token': token.key,
+            'username': user.username,
+            'email': user.email
+        })
     
